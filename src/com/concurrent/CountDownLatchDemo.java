e21d7859b22d8cd7fb07aaac65d2afe9870e2478
yinchao <291355504@qq.com>
2019-03-21 23:29:08 +0800

    Java并发工具类，CountDownLatch使用

---
+++ src/com/concurrent/CountDownLatchDemo.java
@@ -0,0 +1,40 @@
+package com.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+
+	private static CountDownLatch countDownLatch = new CountDownLatch(5);
+
+	static class BossThread implements Runnable{
+
+		@Override
+		public void run() {
+			System.out.println("Boss在会议室等待开会，总共有："+countDownLatch.getCount()+"人在等待开会");
+			try {
+				countDownLatch.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("人到齐了，开始开会吧");
+		}
+	}
+
+	static class EmplyeeThread implements Runnable{
+
+		@Override
+		public void run() {
+			System.out.println("线程："+Thread.currentThread().getName()+"到达会场开始开会");
+			countDownLatch.countDown();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Thread(new BossThread()).start();
+
+		for (long i = 0; i < countDownLatch.getCount(); i++) {
+			new Thread(new EmplyeeThread()).start();
+		}
+	}
+
+}
