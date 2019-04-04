555849c6379c3afb219115c2fa4736a0d1ab6530
yinchao <291355504@qq.com>
2019-04-04 08:18:32 +0800

    剑指offer，重建二叉树

--- src/com/algorithm/Demo4.java
+++ src/com/algorithm/Demo4.java
@@ -8,8 +8,33 @@ import com.algorithm.auxiliary.TreeNode;
  */
 public class Demo4 {
 
-    public TreeNode reConstructBinaryTree(int [] pre, int [] in) {
-        return null;
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        return reConstruct(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+
+    private TreeNode reConstruct(int[] pre, int preBegin, int preEnd, int[] in, int inBegin, int inEnd) {
+        if (preBegin > preEnd || inBegin > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[preBegin]);
+        for (int i = inBegin; i <= inEnd; i++) {
+            if (in[i] == pre[preBegin]) {
+                root.left = reConstruct(pre, preBegin + 1, preEnd, in, inBegin, i - 1);
+                root.right = reConstruct(pre, preBegin + 1, preEnd, in, i + 1, inEnd);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+
+        Demo4 demo = new Demo4();
+
+        TreeNode head = demo.reConstructBinaryTree(pre, in);
+        System.out.println("ceshi");
     }
 
 }
