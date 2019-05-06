c7ab4f3a0b5961900ad971516ad81fd3521f0e3b
yinchao <291355504@qq.com>
2019-05-06 23:35:33 +0800

    Java集合源码解析

--- src/com/frame_work/ConcurrentHashMapDemo.java
+++ src/com/frame_work/ConcurrentHashMapDemo.java
@@ -3,16 +3,22 @@ package com.frame_work;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ConcurrentHashMapDemo{
+public class ConcurrentHashMapDemo {
 
-    private static Map<String,String> conMap = new ConcurrentHashMap<>();
-
-    public static void testPut(){
-        conMap.put("1","ceshi");
+    public void testConcurrentHashMapDemo() {
+        Map<String, String> map = new ConcurrentHashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.print(entry.getKey()+"===");
+            System.out.println(entry.getValue());
+        }
     }
 
 
     public static void main(String[] args) {
-        int a = new Integer(1).hashCode();
+        ConcurrentHashMapDemo hashMapDemo = new ConcurrentHashMapDemo();
+        hashMapDemo.testConcurrentHashMapDemo();
     }
 }
--- src/com/frame_work/MapDemo.java
+++ src/com/frame_work/MapDemo.java
@@ -4,24 +4,47 @@ import java.util.*;
 
 public class MapDemo {
 
-    public  void testMapDemo(){
+    public  void testHashMapDemo(){
         Map<String,String> map = new HashMap<String,String>(4);
-        map.put("1","2");
-        map.put("1","3");
-        for(Map.Entry<String,String> entry : map.entrySet()){
+        map.put("1","1");
+        map.put("5","5");
+        map.put("4","4");
+        map.put("2","2");
+        map.put("3","3");
+
+        for(Map.Entry<String,String> entry:map.entrySet()){
             System.out.print(entry.getKey()+"==");
             System.out.println(entry.getValue());
         }
+
     }
 
     public void testLinkedHashMap(){
         Map<String,String> map = new LinkedHashMap<String,String>();
-        map.put("1","2");
+        map.put("1","1");
+        map.put("5","5");
+        map.put("4","4");
+        map.put("2","2");
+        map.put("3","3");
         for(Map.Entry<String,String> entry:map.entrySet()){
             System.out.print(entry.getKey()+"==");
             System.out.println(entry.getValue());
         }
     }
+
+    public void testTreeMap(){
+        Map<String,String> map = new TreeMap<String,String>();
+        map.put("1","1");
+        map.put("5","5");
+        map.put("4","4");
+        map.put("2","2");
+        map.put("3","3");
+        for(Map.Entry<String,String> entry:map.entrySet()){
+            System.out.print(entry.getKey()+"==");
+            System.out.println(entry.getValue());
+        }
+    }
+
     static final int hash(Object key) {
         int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
@@ -29,10 +52,9 @@ public class MapDemo {
 
     public static void main(String[] args) {
         MapDemo mapDemo = new MapDemo();
-        mapDemo.testMapDemo();
-        System.out.println(hash("1"));
-        int h = "5".hashCode();
-        System.out.println((h^h>>>16)&3);
+//        mapDemo.testLinkedHashMap();
+//        mapDemo.testHashMapDemo();
+        mapDemo.testTreeMap();
     }
 
 
