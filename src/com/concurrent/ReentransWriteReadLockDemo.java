2ec4801c017c3b38f13d8a3096eb8d97bf4f7c85
yinchao <291355504@qq.com>
2019-09-04 23:44:28 +0800

    Java 算法 二

---
+++ src/com/concurrent/ReentransWriteReadLockDemo.java
@@ -0,0 +1,32 @@
+package com.concurrent;
+
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentransWriteReadLockDemo {
+	private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+	static class IncrementVauleThread implements Runnable {
+
+		@Override
+		public void run() {
+
+			lock.writeLock().lock();
+			lock.readLock().lock();
+			for (int i = 0; i < 10; i++) {
+				System.out.println("线程：" + Thread.currentThread().getName()+"---"+i);
+			}
+			lock.readLock().unlock();
+			lock.writeLock().unlock();
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+		for(int i=0;i<10;i++){
+			new Thread(new IncrementVauleThread()).start();
+		}
+
+	}
+}
