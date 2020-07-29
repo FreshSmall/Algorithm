128405857d7882fe96b37f18ba3563efa56322d3
yinchao <ycsuper2819@gmail.com>
2020-07-29 11:00:59 +0800

    分支master提交

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,6 +37,10 @@ public class Main {
         System.out.println("测试main地址");
     }
 
+    public void testMethod8(){
+        System.out.println("测试main地址");
+    }
+
     public static void main(String[] args) throws InterruptedException {
 
         final Demo d = new Demo();
