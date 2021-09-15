df0504625209c45f983cc9895cfdd554d08ecac6
yinchao <ycsuper2819@gmail.com>
2021-09-15 20:37:06 +0800

    增加asm

---
+++ src/main/java/com/asm/HelloWorld.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) GSX Techedu Inc. All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.asm;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author yinchao
+ * @description
+ * @team wuhan operational dev.
+ * @date 2021/9/15 19:49
+ **/
+public class HelloWorld {
+
+    /**
+     * asm 实现
+     *
+     */
+    private static byte[] generate(){
+        ClassWriter classWriter = new ClassWriter(0);
+        // 定义对象头；版本号、修饰符、全类名、签名、父类、实现的接口
+        classWriter.visit(Opcodes.V1_7, Opcodes.ACC_PUBLIC, "com/asm/HelloWorld", null, "java/lang/Object", null);
+        // 添加方法；修饰符、方法名、描述符、签名、异常
+        MethodVisitor methodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        // 执行指令；获取静态属性
+        methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        // 加载常量 load constant
+        methodVisitor.visitLdcInsn("Hello World");
+        // 调用方法
+        methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+        // 返回
+        methodVisitor.visitInsn(Opcodes.RETURN);
+        // 设置操作数栈的深度和局部变量的大小
+        methodVisitor.visitMaxs(2, 1);
+        // 方法结束
+        methodVisitor.visitEnd();
+        // 类完成
+        classWriter.visitEnd();
+        // 生成字节数组
+        return classWriter.toByteArray();
+    }
+
+    public static void main(String[] args) {
+        // System.out.println("hello world！");
+        // 生成二进制字节码
+        byte[] bytes = generate();
+        // 输出字节码
+        outputClazz(bytes);
+        // 加载AsmSumOfTwoNumbers
+        GenerateSumOfTwoNumbers generateSumOfTwoNumbers = new GenerateSumOfTwoNumbers();
+        Class<?> clazz = generateSumOfTwoNumbers.defineClass("org.itstack.demo.asm.AsmSumOfTwoNumbers", bytes, 0, bytes.length);
+        // 反射获取 main 方法
+        Method method = clazz.getMethod("sum", int.class, int.class);
+        Object obj = method.invoke(clazz.newInstance(), 6, 2);
+        System.out.println(obj);
+    }
+
+}
